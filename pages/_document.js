548591bee34b66c827ac81361aad5fd768464fbf
John Paul Mariano <jppm8387@yahoo.com>
2022-12-21 08:13:42 -0500

    Error when extending a document

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document , { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
 
 export default class MyDocument extends Document {
   static async getInitialProps (ctx) {
@@ -27,3 +28,4 @@ export default class MyDocument extends Document {
     }
   }
 }
+
