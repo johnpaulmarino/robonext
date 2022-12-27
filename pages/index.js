890c6357afe755725de0f0f14716fef63bc4e11f
John Paul Mariano <jppm8387@yahoo.com>
2022-12-27 14:42:57 -0500

    testing config vars

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Robocalls from 'components/robocalls';
 
 //GATSBY_BASE_URL
 export default function Home() {
+  console.log(process.env);
   return (
     <>
       {process.env.GATSBY_PAGE === 'RESILIENCY' ? (
